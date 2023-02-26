fd56c4b7596740c3d1d4c47cc1bcc2021defa25b
VSushkov <resid.marketing@gmail.com>
2023-02-26 16:38:54 +0200

    modify script.js

--- js/script.js
+++ js/script.js
@@ -80,21 +80,6 @@ $(document).ready(function () {
     });
 });
 
-$('.carousel__inner').slick({
-    speed: 1200,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/arr_left.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/arr_right.png"></button>',
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                arrows: false
-            }
-        },
-
-    ]
-});
-
 
 jQuery(function ($) {
     $('.phone').mask("+38(999) 999-99-99");
